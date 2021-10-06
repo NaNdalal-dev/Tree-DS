f47e3c8fa084affd67941b94f4d9eb6f8116b385
NaNdalal-dev <dnandalal7@gmail.com>
2021-10-06 10:25:20 +0530

    Added 2 Traversal methods (In and Pre order)

--- bst.py
+++ bst.py
@@ -35,13 +35,33 @@ class BinarySearchTree:
 				self.right.search(value)
 			else:
 				print(f"Node {value} not found.")
+	#Traversal
+	def preorder(self):
+		print(self.key)
+		if self.left:
+			self.left.preorder()
+		if self.right:
+			self.right.preorder()
+
+	def inorder(self):
+		
+		if self.left:
+			self.left.inorder()
+
+		print(self.key)
+
+		if self.right:
+			self.right.inorder()
 
 	def __repr__(self):	
 		return f"{self.key}"
 
-a = BinarySearchTree(10)
-for val in [1,5,2,56,12,56,121,-1]:
+a = BinarySearchTree(21)
+for val in [10,30,5,12,25,100,3,7]:
 	a.insert(val)
 
-for val in [1,5,2,56,12,56,121,-1]:
-	a.search(val)
+#for val in [1,5,2,56,12,56,121,-1]:
+#	a.search(val)
+
+#a.preorder()
+a.inorder()
